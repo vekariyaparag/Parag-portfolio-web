65c550f68bd4a0a22aca3ac9514329c435082c19
vekariyaparag <vekariyaparag07@gmail.com>
2025-02-01 17:50:05 +0530

    script update

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
-.dropbtn {
-    background-color: black;
-    color: white;
-    opacity: 58%;
-    padding: 16px;
-    font-size:25px;
-    border: none;
-    cursor: pointer;
+/*document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed('#domains', {
+    strings: [' ', 'Software Engineer.', 'Python Developer.', 'Student..'],
+    typeSpeed: 40,
+    backspeed: 50,
+    loop: true
+  });
+});
+*/
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
-  
-  
-  /* Dropdown button on hover & focus */
-  .dropbtn:hover, .dropbtn:focus {
-    background-color: dodgerblue;
-  }
-  
-  /* The container <div> - needed to position the dropdown content */
-  .dropdown {
-    position: relative;
-    display: inline-block;
-  }
-  
-  /* Dropdown Content (Hidden by Default) */
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: black;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-  
-  /* Links inside the dropdown */
-  .dropdown-content a {
-    color: white;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-  }
-  
-  /* Change color of dropdown links on hover */
-  .dropdown-content a:hover {background-color: bla;}
-  
-  /* Show the dropdown menu (use JS to add this class to the .dropdown-content container when the user clicks on the dropdown button) */
-  .show {display:block;}
+}
